a44f62c92e2925c7541ccc8e28c62c594de38628
Mathias Fritsch <70696381+Matze9@users.noreply.github.com>
2022-05-01 20:54:58 +0200

    Added Project Template Files;

---
+++ Assets/Scripts/BlanketIsLifted.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BlanketIsLifted : MonoBehaviour
+{
+
+   public Transform patient;
+    public Transform blanket;
+    bool x = false;
+
+
+    bool IsMoved = false;
+
+    public void moveCheck()
+
+      
+    {
+        x = true;
+        
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+         
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+       if (x == true)
+        {
+            patient.Rotate(0, 0, 50 * Time.deltaTime);
+        }
+
+
+        
+    }
+}
+
+
